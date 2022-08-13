d6fd1e780257ba2f5558d2795f3407bb1eeb8f49
Adem  Kurşun <adem.kursun@trendyol.com>
2022-08-13 12:57:33 +0300

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+import React, { useState } from "react";
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div className="flex h-screen">
+            <div className="m-auto">
+                <div className="text-6xl text-red-600">{count}</div>
+                <button className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white" type="button" onClick={() => setCount((count) => count + 1)}>
+                    count+
+                </button>
+            </div>
+        </div>
+    )
 }
-
-export default App
+export default Counter;
