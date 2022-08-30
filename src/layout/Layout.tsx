1c4cbf90ccba9b9906620e1b37d37a8fb139dcf8
Mert Can Altın <mertgold60@gmail.com>
2022-08-30 14:55:00 +0300

    prettify

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,38 +5,35 @@ import UserSidebar from "./sidebar/userSidebar";
 import TopBar from "./topbar/topBar";
 
 const Layout = () => {
-    return (
-       
+	return (
+		<div className='flex'>
+			{/* Icon left sidebar */}
+			{/* <IconSideBar /> */}
+			<div className='flex flex-1 flex-col min-h-screen h-screen '>
+				{/* Top bar */}
+				<TopBar />
+				{/* End of top bar */}
 
-<div className='flex'>
-{/* Icon left sidebar */}
-{/* <IconSideBar /> */}
-<div className='flex flex-1 flex-col min-h-screen h-screen '>
-  {/* Top bar */}
-  <TopBar />
-  {/* End of top bar */}
+				{/* Body */}
+				<div className='flex-1 bg-blue-500 flex overflow-y-hidden'>
+					{/* Left side bar */}
+					<GuildsNav />
+					<FriendsNav />
+					{/* End of left sidebar */}
+					<div className='flex-1 flex justify-between'>
+						{/* Main content area */}
+						<ContentBody />
 
-  {/* Body */}
-  <div className='flex-1 bg-blue-500 flex overflow-y-hidden'>
-	{/* Left side bar */}
-	
-	<GuildsNav />
-            <FriendsNav />
-	{/* End of left sidebar */}
-	<div className='flex-1 flex justify-between'>
-	  {/* Main content area */}
-	  <ContentBody />
-	  
-	  {/* End of main content */}
-	  {/* User sidebar */}
-	  <UserSidebar />
-	  
-	  {/* End of user sidebar */}
-	</div>
-  </div>
-  {/* Body */}
-</div>
-</div>
-    )
+						{/* End of main content */}
+						{/* User sidebar */}
+						<UserSidebar />
+
+						{/* End of user sidebar */}
+					</div>
+				</div>
+				{/* Body */}
+			</div>
+		</div>
+	)
 }
 export default Layout;
